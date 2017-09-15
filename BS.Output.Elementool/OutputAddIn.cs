81856916897a75541f8087450322a822c57c694b
Alexej Hirsch <alexej@bugshooting.com>
2017-09-15 21:45:25 +0200

    Projektdateien hinzufügen.

---
+++ BS.Output.Elementool/OutputAddIn.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.Web;
+using System.Threading.Tasks;
+
+namespace BS.Output.Elementool
+{
+  public class OutputAddIn: V3.OutputAddIn<Output>
+  {
+
+    protected override string Name
+    {
+      get { return "elementool"; }
+    }
+
+    protected override Image Image64
+    {
+      get  { return Properties.Resources.logo_64; }
+    }
+
+    protected override Image Image16
+    {
+      get { return Properties.Resources.logo_16 ; }
+    }
+
+    protected override bool Editable
+    {
+      get { return true; }
+    }
+
+    protected override string Description
+    {
+      get { return "Attach screenshots to elementool issues."; }
+    }
+    
+    protected override Output CreateOutput(IWin32Window Owner)
+    {
+      
+      Output output = new Output(Name, 
+                                 String.Empty, 
+                                 String.Empty, 
+                                 String.Empty, 
+                                 "Screenshot",
+                                 String.Empty, 
+                                 true,
+                                 "1");
+
+      return EditOutput(Owner, output);
+
+    }
+
+    protected override Output EditOutput(IWin32Window Owner, Output Output)
+    {
+
+      Edit edit = new Edit(Output);
+
+      var ownerHelper = new System.Windows.Interop.WindowInteropHelper(edit);
+      ownerHelper.Owner = Owner.Handle;
+      
+      if (edit.ShowDialog() == true) {
+
+        return new Output(edit.OutputName,
+                          edit.AccountName,
+                          edit.UserName,
+                          edit.Password,
+                          edit.FileName,
+                          edit.FileFormat,
+                          edit.OpenItemInBrowser,
+                          Output.LastIssueNumber);
+      }
+      else
+      {
+        return null; 
+      }
+
+    }
+
+    protected override OutputValueCollection SerializeOutput(Output Output)
+    {
+
+      OutputValueCollection outputValues = new OutputValueCollection();
+
+      outputValues.Add(new OutputValue("Name", Output.Name));
+      outputValues.Add(new OutputValue("AccountName", Output.AccountName));
+      outputValues.Add(new OutputValue("UserName", Output.UserName));
+      outputValues.Add(new OutputValue("Password",Output.Password, true));
+      outputValues.Add(new OutputValue("OpenItemInBrowser", Convert.ToString(Output.OpenItemInBrowser)));
+      outputValues.Add(new OutputValue("FileName", Output.FileName));
+      outputValues.Add(new OutputValue("FileFormat", Output.FileFormat));
+      outputValues.Add(new OutputValue("LastIssueNumber", Output.LastIssueNumber));
+
+      return outputValues;
+      
+    }
+
+    protected override Output DeserializeOutput(OutputValueCollection OutputValues)
+    {
+
+      return new Output(OutputValues["Name", this.Name].Value,
+                        OutputValues["AccountName", ""].Value, 
+                        OutputValues["UserName", ""].Value,
+                        OutputValues["Password", ""].Value, 
+                        OutputValues["FileName", "Screenshot"].Value, 
+                        OutputValues["FileFormat", ""].Value,
+                        Convert.ToBoolean(OutputValues["OpenItemInBrowser", Convert.ToString(true)].Value),
+                        OutputValues["LastIssueNumber", "1"].Value);
+
+    }
+
+    protected override async Task<V3.SendResult> Send(IWin32Window Owner, Output Output, V3.ImageData ImageData)
+    {
+
+      try
+      {
+
+        // TODO
+        return null;
+        
+      }
+      catch (Exception ex)
+      {
+        return new V3.SendResult(V3.Result.Failed, ex.Message);
+      }
+
+    }
+      
+  }
+}
